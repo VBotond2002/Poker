7436ed2e57d64dcee9ec14b99b3200ff75b221ea
VBotond2002 <vastag.botond@gmail.com>
2022-05-01 09:47:08 +0200

    main.cpp edited

--- default/main.cpp
+++ default/main.cpp
@@ -1,10 +1,10 @@
 #include "graphics.hpp"
 using namespace genv;
 
-
+const unsigned int _W=600,_H=400;
 int main()
 {
-    gout.open(400,400);
+    gout.open(_W,_H);
     gout<<refresh;
     event ev;
     while(gin >> ev) {
